cd09d91fd4b7e2e8b25aa06988e318ec2e80f056
Jesse Marino <100870863+justbytes@users.noreply.github.com>
2023-01-29 16:57:00 -0800

    contract complete and compiles

--- contracts/DumpsterFire.sol
+++ contracts/DumpsterFire.sol
@@ -1,15 +1,51 @@
 //contracts/DumpsterFire.sol
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract DumpsterFire is ERC20 {
+
+contract DumpsterFire is ERC20Capped, ERC20Burnable {
 
     address payable public owner;
+    uint256 public blockReward;
 
-    constructor(uint256 initialSupply) ERC20("DumpsterFire Token", "DFT") {
-        owner = msg.sender;
-        _mint(owner, initialSupply);
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
     }
+
+    constructor(uint256 cap, uint256 reward) ERC20("DumpsterFire", "DFT") ERC20Capped(cap * (10 ** decimals())) {
+        owner = payable(msg.sender);
+        _mint(owner, 70000000 * (10 ** decimals()));
+        blockReward = reward * (10 ** decimals());
+    }  
+
+    function _mint(address account, uint256 amount) internal virtual override(ERC20Capped, ERC20) {
+        require(ERC20.totalSupply() + amount <= cap(), "ERC20Capped: cap exceeded");
+        super._mint(account, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 value) internal virtual override {
+        
+        if(from != address(0) && to != block.coinbase && block.coinbase != address(0)) {
+            _mintMinerReward();
+        }
+        super._beforeTokenTransfer(from, to, value);
+    }
+
+    function _mintMinerReward() internal {
+        _mint(block.coinbase, blockReward);
+    }
+
+    function setBlockReward(uint256 reward) public onlyOwner {
+        blockReward = reward * (10 ** decimals());
+    }
+
+    function destroy() public onlyOwner {
+        selfdestruct(owner);
+    }
+
 }
--- test/DumpsterFIre.js
+++ test/DumpsterFIre.js
@@ -1,1 +1,2 @@
-
+const { expect } = require("chai");
+const hre = require("hardhat");
