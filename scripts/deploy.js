f0b509f4eb5ba253fabf61b7c6b3191b94173e01
Jesse Marino <100870863+justbytes@users.noreply.github.com>
2023-01-30 19:36:13 -0800

    deployed contract to goerli test network

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,26 +1,11 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const DumpsterFire = await hre.ethers.getContractFactory("DumpsterFire");
+  const dumpsterFire = await DumpsterFire.deploy(100000000, 50);
+  await dumpsterFire.deployed();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log(`Dumpster Fire deployed at:${dumpsterFire.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
